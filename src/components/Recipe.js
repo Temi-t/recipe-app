b5c7460e0265a03cc936999adb6a4c1e6cd12bc7
Temi-t <temiemmanuel@yahoo.com>
2022-05-02 20:55:14 +0100

    playing with styles

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -5,7 +5,7 @@ export default function Recipe({ recipe, children }) {
     //tabIndex means the element should be focusable in sequential keyboard navigation.
     <div key={recipe.id} className="recipe" tabIndex={0}>
       <span className="recipe-container">
-        <h2 className="recipe-name">{recipe.name}</h2>
+        <h3 className="recipe-name">{recipe.name}</h3>
         <div className="image-container">
           <img src={recipe.img} alt="delicacy" className="recipe-image" />
           {console.log("image from Recipe: ", recipe.img)}
